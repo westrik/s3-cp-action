cc4891ca7259f2739956d3260bc23ca1787d6cef
TPaschalis <tpaschalis@users.noreply.github.com>
2019-09-10 22:51:52 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET} \
+aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
             --follow-symlinks \
             --delete \
             --region ${AWS_REGION} $*
