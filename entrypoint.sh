cd974c72f855670075aa2afdd65ca0818e2ef1fe
TPaschalis <tpaschalis@users.noreply.github.com>
2019-09-11 09:36:38 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,6 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            --follow-symlinks \
-            --delete \
             --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
